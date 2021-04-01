f98560394fc04d50d70e6fa362fb3fb13ce40027
brenorosas <brenorosas@hotmail.com>
2021-04-01 12:03:41 -0300

    Changes on 4 º and 3º

--- paradigmas/prolog/prova2017.pl
+++ paradigmas/prolog/prova2017.pl
@@ -5,30 +5,34 @@ interseccao([],_,[]) :- !.
 interseccao([X|R], L, [X|W]) :- exist(X,L), interseccao(R,L,W), !.
 interseccao([_|R], L, W) :- interseccao(R,L,W).
 
+%1
 oculte(_,[],[]) :- !.
 oculte(X,[X|R], [xxxx|W]) :- oculte(X,R,W), !.
 oculte(X,[Y|R], [Y|W]) :- oculte(X,R,W).
 
+%2
 oculteConj(_,[],[]) :- !.
 oculteConj(X, [Y|R], [xxxx|W]) :- exist(Y,X), oculteConj(X,R,W), !.
 oculteConj(X,[Y|R],[Y|W]) :- oculteConj(X,R,W).
 
-intercala1(_, _, Y, N, []) :- Y =:= N, !.
-intercala1(X,Y,K, N, [X|W]) :- K mod 2 =:= 0, A is K+1, intercala1(X, Y, A, N,W), !.
-intercala1(X,Y,K, N, [Y|W]) :- A is K+1, intercala1(X,Y,A,N,W).
+%3
+intercala(_,_,0,[]) :- !.
+intercala(X,_,1,[X]) :- !.
+intercala(X,Y,N,[X,Y|W]) :- Z is N-2, intercala(X,Y,Z,W).
 
-intercala(X,Y,N,L) :- intercala1(X,Y,0,N,L).
-
+%4
 cont(_, [], 0) :- !.
 cont(X, [X|R], Y) :- Z is 1, cont(X,R,W), Y is Z+W, !.
 cont(X, [_|R], Y) :- Z is 0, cont(X,R,W), Y is Z+W.
 
-sumarizex(_,[],[],[]) :- !.
-sumarizex(LA,[X|R],L1,L2) :- exist(X,LA), sumarizex(LA,R,L1,L2), !.
-sumarizex(LA,[X|R],[X|W],[Y|W2]) :- cont(X,[X|R],Y), sumarizex([X|LA], R, W, W2).
+remova(_,[],[]) :- !.
+remova(X,[X|T],W) :- remova(X,T,W), !.
+remova(X,[H|T],[H|NT]):-remova(X,T,NT).
 
-sumarize(L,L1,L2) :- sumarizex([],L,L1,L2).
+sumarize([],[],[]) :- !.
+sumarize([X|R], [X|R1], [Y|R2]) :- cont(X,[X|R],Y), remova(X,[X|R],L), sumarize(L,R1,R2).
 
+%5
 make(V,[X],[X],[]) :- V==X, !.
 make(_,[X],[],[X]) :- !.
 make(V,[X|R],[X|W],L) :- V==X, make(V,R,W,L), !.
